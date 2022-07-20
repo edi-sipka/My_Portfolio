cf1b8e3637b86f0c1542c84fcf994418d9b5e775
edi-sipka@gmail.com <edisipka@gmail.com>
2022-07-20 15:25:17 +0100

    Add Dom JS tree

--- popup.js
+++ popup.js
@@ -1,102 +1,87 @@
-const projects = [
-  {
-    name: 'Tonic',
-    img: 'Images/Portfolio.png',
-    tools: ['html', 'css', 'javascript'],
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the released",
-    liveSource: 'https://edi-sipka.github.io/My_Portfolio/',
-    source: 'https://github.com/edi-sipka/My_Portfolio',
-  },
-  {
-    name: 'Tonic',
-    portfolioPhoto: 'Images/Portfolio2.png',
-    tools: ['html', 'css', 'javascript'],
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the released",
-    liveSource: 'https://edi-sipka.github.io/My_Portfolio/',
-    source: 'https://github.com/edi-sipka/My_Portfolio',
-  },
-  {
-    name: 'Multi post Stories',
-    portfolioPhoto: 'Images/Portfolio3.png',
-    tools: ['html', 'css', 'javascript'],
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the released",
-    liveSource: 'https://edi-sipka.github.io/My_Portfolio/',
-    source: 'https://github.com/edi-sipka/My_Portfolio',
-  },
-
-  {
-    name: 'Multi post Stories',
-    portfolioPhoto: 'Images/Portfolio4.png',
-    tools: ['html', 'css', 'javascript'],
-    description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the released",
-    liveSource: 'https://edi-sipka.github.io/My_Portfolio/',
-    source: 'https://github.com/edi-sipka/My_Portfolio',
-  },
-];
-
-projects.forEach((element, i) => {
-  const portfolio = document.querySelector('.portfolio');
-  const navigation = document.createElement('nav');
-  const navigation2 = document.createElement('nav');
-  const image = document.createElement('img');
-  const image2 = document.createElement('img');
-  const image3 = document.createElement('img');
-  const div = document.createElement('div');
-  const h3 = document.createElement('h3');
-  const h4 = document.createElement('h4');
-  const span = document.createElement('span');
-  const span2 = document.createElement('span');
-  const p = document.createElement('p');
-  const ul = document.createElement('ul');
-  const button = document.createElement('button');
-
-  portfolio.classList.add('portfolio');
-
-  navigation.classList.add('portfolio-border');
-  navigation.append(image, div);
-
-  div.classList.add('project-section');
-  div.append(h3, navigation2, p, ul, button);
-
-  navigation2.classList.add('project-info');
-  navigation2.append(h4, image2, span, image2, span);
-
-  image.src = element.portfolioPhoto;
-  image.classList.add('portfolio-image');
-  image.setAttribute('alt', 'Portfolio project');
-
-  h3.classList.add('project-name');
-  h3.innerText = element.name;
-
-  h4.classList.add('project-client');
-  h4.innerText = 'CANOPY';
-
-  image2.src = 'Images/Counter.png';
-  image2.classList.add('counter');
-  image2.setAttribute('alt', 'Dot');
-
-  span.classList.add('programming-language');
-  span.innerText = 'Back End Dev';
-
-  image3.src = 'Images/Counter.png';
-  image3.classList.add('counter');
-  image3.setAttribute('alt', 'Dot');
-
-  span2.classList.add('year');
-  span2.innerText = 2015;
-
-  p.classList.add('project-details');
-  p.innerText =
-    'A daily selection of privately personalized reads; no accounts or sign-ups required.';
-
-  ul.classList.add('languages');
-
-  button.classList.add('btn');
-  button.innerText = 'See Projects';
-
-  portfolio.appendChild(navigation);
+/*window.addEventListener('hashchange', () => {
+  console.log(window.location.hash);
+  projects.forEach((project) => {
+    if (`#${project.id}` === window.location.hash) {
+      console.log(project);
+    }
+  });
 });
+*/
+
+/*
+const projectButton = document.querySelectorAll('.btn');
+const popup = document.querySelector('.background-popup');
+const showPopup = document.querySelector('.popup');
+
+projectButton.forEach((n) => {
+  n.addEventListener('click', (e) => {
+    const elements = e.elements.id;
+    const project = projects[elementsId];
+
+    showPopup.innerHTML = (
+      <div class="card-pop">
+        <div class="flex-popup">
+          <h1 class="section-title">'${project.name}'</h1>
+          <img src="images/canel.png" class="closing-icon" alt="close button" />
+        </div>
+        <div class="project-section">
+          <h4 class="project-client">CANOPY</h4>
+          <img src="images/Counter.png" alt="counter" class="counter" />
+          <span class="programming-language">Back End Dev</span>
+          <img src="images/Counter.png" alt="counter" class="counter" />
+          <span class="year">2015</span>
+        </div>
+        <div class="pic-container">
+          <img
+            src="${project.portfolioPhoto}"
+            alt="card pic"
+            class="cards-pic"
+          />
+        </div>
+
+        <div class="flex-desktop">
+          <p>'${project.description}'</p>
+          <div>
+            <ul class="languages">
+              <li class="${project.tools[0]}">html</li>
+
+              <li class="${project.tools[1]}">css</li>
+
+              <li class="${project.tools[2]}">javascript</li>
+            </ul>
+            <div class="btn-flex">
+              <button>
+                <a
+                  href="${project.liveVersion}"
+                  class="btn-flex1"
+                  alt="Broken Link"
+                >
+                  See Live{' '}
+                  <img
+                    src="images/Brokenlink.png"
+                    alt="Broken link"
+                    class="size dot"
+                  />
+                </a>
+              </button>
+              <button>
+                <a href="${project.source}" class="btn-flex1" alt="Broken Link">
+                  See Source{' '}
+                  <img
+                    src="images/github.png"
+                    alt="github logo"
+                    class="size dot"
+                  />
+                </a>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+
+    popup.append(showPopup);
+    popup.classList.add('visible');
+  });
+});
+*/
---
+++ projects.js
@@ -0,0 +1,84 @@
+import projectData from './data.js';
+console.log(projectData);
+
+const Article = (project, index) => {
+  const article = document.createElement('article');
+  article.className = 'portfolio-border';
+  const img = document.createElement('img');
+  img.className = 'portfolio-image';
+  img.setAttribute('src', project.img);
+  article.appendChild(img);
+
+  const div = document.createElement('div');
+  div.className = 'project-section';
+  article.appendChild(div);
+  if (index % 2 === 1) {
+    div.classList.add('project-section-reverse');
+  }
+
+  const h3 = document.createElement('h3');
+  h3.className = 'project-name';
+  h3.innerText = project.name;
+  div.appendChild(h3);
+
+  const div2 = document.createElement('div');
+  div2.className = 'project-info';
+  div.appendChild(div2);
+
+  const h4 = document.createElement('h4');
+  h4.className = 'project-client';
+  h4.innerText = project.client;
+  div2.appendChild(h4);
+
+  const img2 = document.createElement('img');
+  img2.className = 'counter';
+  img2.setAttribute('src', './Images/Counter.png');
+  div2.appendChild(img2);
+
+  const span = document.createElement('span');
+  span.className = 'programming-language';
+  span.innerText = 'Back End Dev';
+  div2.appendChild(span);
+
+  const img3 = document.createElement('img');
+  img3.className = 'counter';
+  img3.setAttribute('src', './Images/Counter.png');
+  div2.appendChild(img3);
+
+  const span2 = document.createElement('span');
+  span2.className = 'year';
+  span2.innerText = project.year;
+  div2.appendChild(span2);
+
+  const p = document.createElement('p');
+  p.className = 'project-details';
+  p.innerText = project.paragraph;
+  div.appendChild(p);
+
+  const ul = document.createElement('ul');
+  ul.className = 'languages';
+  div.appendChild(ul);
+
+  project.technologies.forEach((technology) => {
+    const li = document.createElement('li');
+    li.className = 'dev-tool';
+    li.innerText = technology;
+    ul.appendChild(li);
+  });
+  const button = document.createElement('button');
+  button.className = 'btn';
+  button.innerText = 'See Project';
+  div.appendChild(button);
+
+  return article;
+};
+
+const projects = document.createElement('div');
+projects.className = 'portfolio';
+
+projectData.forEach((project, index) => {
+  projects.appendChild(Article(project, index));
+});
+
+const section = document.querySelector('#portfolio');
+section.appendChild(projects);
