e252c8a7eed136beb49c6e5357336dd2a1fafcf2
gnuion <gnu.ion@gmail.com>
2022-07-21 12:47:33 +0200

    Make popup responsive

--- popup.js
+++ popup.js
@@ -1,12 +1,16 @@
 const Popup = (project) => {
-  const popup = document.createElement('div');
-  popup.className = 'popup';
-
+ 
+  // Backdrop
   const backdrop = document.createElement('div');
   backdrop.className = 'backdrop';
-  backdrop.appendChild(popup);
   document.body.appendChild(backdrop);
 
+  // Popup
+  const popup = document.createElement('div');
+  popup.className = 'popup';
+  backdrop.appendChild(popup);
+
+  // Close button
   const exit = document.createElement('a');
   exit.addEventListener('click', () => {
     backdrop.remove();
@@ -15,74 +19,76 @@ const Popup = (project) => {
   exit.innerHTML = '<img src="./Images/Cancel.png">';
   exit.classList.add('closing');
 
+  // Project title
   const h1 = document.createElement('h1');
   h1.innerText = project.name;
   h1.classList.add('project-name');
   popup.appendChild(h1);
 
+  // Project data
   const div = document.createElement('div');
   div.classList.add('popup-data');
   popup.appendChild(div);
-
   const h4 = document.createElement('h4');
   h4.className = 'project-client';
   h4.innerText = project.client;
   div.appendChild(h4);
-
   const img2 = document.createElement('img');
   img2.className = 'counter';
   img2.setAttribute('src', './Images/Counter.png');
   div.appendChild(img2);
-
   const span = document.createElement('span');
   span.className = 'programming-language';
   span.innerText = 'Back End Dev';
   div.appendChild(span);
-
   const img3 = document.createElement('img');
   img3.className = 'counter';
   img3.setAttribute('src', './Images/Counter.png');
   div.appendChild(img3);
-
   const span2 = document.createElement('span');
   span2.className = 'year';
   span2.innerText = project.year;
   div.appendChild(span2);
 
+
+  // Project image
   const div2 = document.createElement('div');
   div2.classList.add('popup-image');
   popup.appendChild(div2);
-
   const img = document.createElement('img');
   img.className = 'project-image';
   img.setAttribute('src', project.img);
   div2.appendChild(img);
 
-  const div3 = document.createElement('div');
-  div3.classList.add('popup-flex');
-  popup.appendChild(div3);
 
+  // Card body
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('popup-flex');
+  popup.appendChild(cardBody);
+
+  
+  // Project description
   const p = document.createElement('p');
   p.classList.add('popup-paragraph');
   p.innerText = project.description;
-  div3.appendChild(p);
-
-  const div4 = document.createElement('div');
-  div4.classList.add('language-flex');
-  div3.appendChild(div4);
-
+  cardBody.appendChild(p);  
+  
+    // Bottons
+    const buttonContainer = document.createElement('div')
+    buttonContainer.className='button-container'
+    cardBody.appendChild(buttonContainer)
+  
+  // Project tags
   const ul = document.createElement('ul');
-  ul.classList.add('ul-list');
-  div4.appendChild(ul);
+  ul.classList.add('languages');
+  buttonContainer.appendChild(ul);  
+  project.technologies.forEach(technology => {
+    const li = document.createElement('li');
+    li.innerText = technology;
+    ul.appendChild(li);  
+  })
 
-  const li = document.createElement('li');
-  li.classList.add('dev-tool');
-  li.innerText = project.technologies;
-  ul.appendChild(li);
 
-  const div5 = document.createElement('div');
-  div5.classList.add('button-flex');
-  div3.appendChild(div5);
 
   const button = document.createElement('button');
   button.classList.add('btn');
@@ -90,7 +96,7 @@ const Popup = (project) => {
   button.addEventListener('click', function () {
     location = project.liveVersion;
   });
-  div5.appendChild(button);
+  buttonContainer.appendChild(button);
 
   const button2 = document.createElement('button');
   button2.classList.add('btn');
@@ -98,7 +104,7 @@ const Popup = (project) => {
   button2.addEventListener('click', function () {
     location = project.source;
   });
-
-  div5.appendChild(button2);
+  buttonContainer.appendChild(button2);
 };
+
 export default Popup;
--- projects.js
+++ projects.js
@@ -1,14 +1,18 @@
 import projectData from './data.js';
-import popup from './popup.js';
+import Popup from './popup.js';
 
 const Article = (project, index) => {
+
   const article = document.createElement('article');
   article.className = 'portfolio-border';
+
+
   const img = document.createElement('img');
   img.className = 'portfolio-image';
   img.setAttribute('src', project.img);
   article.appendChild(img);
 
+  
   const div = document.createElement('div');
   div.className = 'project-section';
   article.appendChild(div);
@@ -69,7 +73,7 @@ const Article = (project, index) => {
   button.className = 'btn';
   button.innerText = 'See Project';
   button.addEventListener('click', () => {
-    popup(project);
+    Popup(project);
   });
   div.appendChild(button);
 
